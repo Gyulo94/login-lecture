041ea84174a432b02c9104b224c5af45961043c8
Gyulo94 <qkrcjfrb112@gmail.com>
2023-12-14 20:34:45 +0900

    el primer cambio

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const home = require('./routes/home')
+// *앱 세팅  //
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use('/', home) // use -> 미들웨어를 등록해주는 메서드
+
+module.exports = app;
---
+++ bin/www.js
@@ -0,0 +1,7 @@
+const app = require('../app')
+port = 3000
+
+app.listen(port, () => {
+    console.log(`서버 가동 중 PORT : ${port}`);
+})
+
